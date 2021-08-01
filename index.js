502c18ab9ee91c342962c75c57150beeedf3b681
Kaustubh72 <ks04071999@gmail.com>
2021-08-01 17:15:48 +0530

    fully functional Cloning Website

--- index.js
+++ index.js
@@ -2,25 +2,60 @@
 const scrape = require('website-scraper');
 const PuppeteerPlugin = require('website-scraper-puppeteer');
 const path = require('path');
+const express =require("express")
+const bodyparser = require("body-parser")
+var cons = require('consolidate');
+const app= express()
 
-scrape({
-    // Provide the URL(s) of the website(s) that you want to clone
-    // In this example, you can clone the Our Code World website
-    urls: ['https://kaustubhsingh.herokuapp.com/'],
-    // Specify the path where the content should be saved
-    // In this case, in the current directory inside the ourcodeworld dir
-    directory: path.resolve(__dirname, 'cloned'),
-    // Load the Puppeteer plugin
-    plugins: [ 
-        new PuppeteerPlugin({
-            launchOptions: { 
-                // If you set  this to true, the headless browser will show up on screen
-                headless: true
-            }, /* optional */
-            scrollToBottom: {
-                timeout: 10000, 
-                viewportN: 10 
-            } /* optional */
-        })
-    ]
+app.engine('html', cons.swig)
+app.set('view engine', 'html');
+
+
+app.use('/',express.static(path.join(__dirname,'views')))
+app.use(bodyparser.json())
+
+app.use(bodyparser.urlencoded({
+    extended: true
+  }));
+
+  app.get("/",(req,res) =>{
+    res.render("index.html")
 });
+
+app.post('/clone', async (req,res) => {
+    
+    const uri=req.body.website;
+    const path_to_save=req.body.path;
+    const pts= path_to_save+"\\cloned";
+    //return res.json({url : uri,path :path_to_save})
+    //return res.json({url : uri});
+    await scrape({
+        // Provide the URL(s) of the website(s) that you want to clone
+        // In this example, you can clone the Our Code World website
+        urls: [uri],
+        // Specify the path where the content should be saved
+        // In this case, in the current directory inside the cloned dir
+        directory: pts,
+        // Load the Puppeteer plugin
+        plugins: [ 
+            new PuppeteerPlugin({
+                launchOptions: { 
+                    // If you set  this to true, the headless browser will show up on screen
+                    headless: true
+                }, /* optional */
+                scrollToBottom: {
+                    timeout: 10000, 
+                    viewportN: 10 
+                } /* optional */
+            })
+        ]
+    });
+
+    res.render("success.html")
+    
+})
+
+
+app.listen(process.env.PORT || 3000, () =>{
+    console.log("Server is Running at Port 3000")
+})
